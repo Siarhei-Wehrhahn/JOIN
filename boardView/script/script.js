51ee155a03f81dd27a3f55aab2b514f342d58457
Sergej <sergei1997.sw@gmail.com>
2024-10-25 18:46:45 +0200

    Overlay mit html nd css weitgehen fertig jetzt werden die JavaScipt funktionen gemacht

--- boardView/script/script.js
+++ boardView/script/script.js
@@ -1,3 +1,5 @@
+let subtaskArray = [];
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -18,45 +20,35 @@ function selectPriority(priority) {
   const urgentButton = document.getElementById('urgent-button');
   const urgentText = document.getElementById('urgentText');
   const urgentIcon = document.getElementById('urgentIcon');
-  
   const mediumButton = document.getElementById('medium-button');
   const mediumText = document.getElementById('mediumText');
   const mediumIcon = document.getElementById('mediumIcon');
-  
   const lowButton = document.getElementById('low-button');
   const lowText = document.getElementById('lowText');
   const lowIcon = document.getElementById('lowIcon');
-  
   if (selectedPriority === priority) {
     urgentButton.classList.remove('redButton');
     urgentText.classList.remove('whiteText');
     urgentIcon.classList.remove('whiteIcon');
-    
     mediumButton.classList.remove('orangeButton');
     mediumText.classList.remove('whiteText');
     mediumIcon.classList.remove('whiteIcon');
-    
     lowButton.classList.remove('greenButton');
     lowText.classList.remove('whiteText');
     lowIcon.classList.remove('whiteIcon');
-    
     selectedPriority = null;
     console.log('Priority deselected');
     return;
   }
-  
   urgentButton.classList.remove('redButton');
   urgentText.classList.remove('whiteText');
   urgentIcon.classList.remove('whiteIcon');
-  
   mediumButton.classList.remove('orangeButton');
   mediumText.classList.remove('whiteText');
   mediumIcon.classList.remove('whiteIcon');
-  
   lowButton.classList.remove('greenButton');
   lowText.classList.remove('whiteText');
   lowIcon.classList.remove('whiteIcon');
-  
   if (priority === 'urgent') {
     urgentButton.classList.add('redButton');
     urgentText.classList.add('whiteText');
@@ -70,9 +62,49 @@ function selectPriority(priority) {
     lowText.classList.add('whiteText');
     lowIcon.classList.add('whiteIcon');
   }
-  
   selectedPriority = priority;
   console.log(`Selected priority: ${selectedPriority}`);
+}
+
+const toggleContactSelection = () => {
+  const contentArea = document.getElementById('contactRender');
+  const arrow = document.querySelector('.dropDownArrow')
+  if(contentArea.classList.contains('d_none')) {
+    contentArea.classList.toggle('d_none');
+    arrow.classList.toggle('turnArrow');
+    renderAddTaskOverlay();
+  } else {
+    contentArea.classList.toggle('d_none');
+    arrow.classList.toggle('turnArrow');
+  }
+}
+
+const renderAddTaskOverlay = async () => {
+  const loadUsers = await loadData('/contacts');
+  const contacts = Object.values(loadUsers)
+  .filter(contact => contact.name && contact.email && contact.phone)
+  .sort((a, b) => a.name.localeCompare(b.name));
+  const overlay = document.getElementById('contactRender');
+  overlay.innerHTML = "";
+
+  for (let index = 0; index < contacts.length; index++) {
+    const person = contacts[index];
+    const initials = person.name.split(' ').slice(0, 2).map(n => n[0]).join('');
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    overlay.innerHTML += getOverlayAddTask(person, initials, randomColor);
+  }
+}
+
+const getOverlayAddTask = (user, initials, color) => {
+  return /*html*/`
+            <div class="contact">
+              <p id="initialsOverlay" style="background-color: ${color};">${initials}</p>
+              <p id="contactName">${user.name}</p>
+              <form>
+                <input type="checkbox" id="exampleCheckbox" name="exampleCheckbox">
+              </form>
+            </div>
+      `;
 }
 
 function toggleAddTaskOverlay() {
@@ -91,25 +123,33 @@ function toggleAddTaskOverlay() {
   }
 }
 
-const renderAddTaskOverlay = () => {
-  const overlay = document.getElementById('contactRender');
-  overlay.innerHTML = "";
+function triggerInputButtons() {
+  const inputField = document.getElementById('subtaskInputId');
+  const buttons = document.getElementById('deleteOrSave');
+  const addIcon = document.getElementById('addIconId');
 
-  users.forEach(user => {
-    const initials = user.name.split(' ').slice(0, 2).map(n => n[0]).join('');
-    const randomColor = colors.randomElement();
-    overlay.innerHTML += getOverlayAddTask(user, initials, randomColor);
-  });
+  if (inputField.value.length > 0) {
+    buttons.classList.remove('d_none');
+    buttons.classList.add('deleteOrSaveBtn');
+    addIcon.classList.add('d_none');
+  } else if(inputField.value.length <= 0) {
+    buttons.classList.add('d_none');
+    buttons.classList.remove('deleteOrSaveBtn');
+    addIcon.classList.remove('d_none');
+  }
 }
 
-const getOverlayAddTask = (user, initials, color) => {
-  return /*html*/`
-            <div class="contact">
-              <p id="initialsOverlay" style="background-color: ${color}; border-radius: 50%;">${initials}</p>
-              <p id="contactName">${user.name}</p>
-              <form>
-                <input type="checkbox" id="exampleCheckbox" name="exampleCheckbox">
-              </form>
-            </div>
-      `;
+const deleteInput = () => {
+  document.getElementById('subtaskInputId').value = "";
+  triggerInputButtons();
+}
+
+const addSubtaskToArray = () => {
+  const inputField = document.getElementById('subtaskInputId').value;
+  subtaskArray.push(inputField);
+  deleteInput();
+}
+
+const renderSubtask = () => {
+  
 }
