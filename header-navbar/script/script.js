535b4c2c76728cb8be09358b35c33e61cb5a7434
Sergej <sergei1997.sw@gmail.com>
2024-10-30 09:54:12 +0100

    Sicherheitscommit alles funktioniert fehlerfrei

--- header-navbar/script/script.js
+++ header-navbar/script/script.js
@@ -146,6 +146,47 @@ const renderContactExtendet = async (index) => {
     content.innerHTML += getContactExtended(person, initials, color);
 }
 
+<<<<<<< HEAD
+=======
+const getContactExtended = (person, initials, color) => {
+    return /*html*/`
+    <div class="contactExtendDiv">
+        <div class="contactName">
+            <div class="contactPhotoExtendedDiv">
+                <div class="contactInitialsExtended" style="background-color: ${color};">${initials}</div>
+            </div>
+            <div class="rightSectionContact">
+                <div class="contactName">
+                    <p class="personNameExtended">${person.name}</p>
+                </div>
+            <div class="singleContactButtons">
+                <div class="singleContactEdit" onclick="toggleEditOverlay('${person.name}', '${initials}', '${color}')">
+                    <img onclick="renderOverlay()" src="../assets/icon/edit.svg" alt="edit pic">
+                    <p>Edit</p>
+                </div>
+                <div class="singleContactEdit" onclick="deleteContact('${person.name}')">
+                    <img src="../assets/icon/delete.svg" alt="trashcan">
+                    <p>Delete</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="contactInfo">
+
+        </div>
+        <div class="contactInformation">
+        <h3 class="contactInfoHeader">Contact Information</h3>
+        <div class="contactMailAndPhone">
+            <h3>Email</h3>
+            <p class="emailAdress">${person.email}</p>
+            <h3>Phone</h3>
+            <p class="phoneNumber">${person.phone}</p>
+        </div>
+        </div>
+    </div>`
+};
+
+>>>>>>> ec5b7cf (Sicherheitscommit alles funktioniert fehlerfrei)
 const deleteContact = async (person) => {
     const selectedPerson = contactsArray.find(contact => contact.name === person);
 
@@ -180,8 +221,7 @@ window.onload = () => {
     loadAddTaskView();
 };
 
-// TODO: grauen hintergrund rein machen
-function toggleEditOverlay(name) {
+function toggleEditOverlay(name, initials, color) {
     const overlay = document.getElementById('overlayEditContact');
     const rightSide = document.getElementById('rightSideId');
     const leftSide = document.getElementById('leftSideId');
@@ -190,7 +230,7 @@ function toggleEditOverlay(name) {
         overlay.classList.remove('d_none');
         overlay.classList.remove('slide-out');
         overlay.classList.add('slide-in');
-        renderEditContactOverlay(name);
+        renderEditContactOverlay(name, initials, color);
     } else {
         overlay.classList.remove('slide-in');
         overlay.classList.add('slide-out');
