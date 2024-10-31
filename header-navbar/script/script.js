a5b9fdb818c2b0ff391911ae456e959b9ce6ebb7
Jason-bxt <jasonpeters355@gmail.com>
2024-10-31 16:08:58 +0100

    push

--- header-navbar/script/script.js
+++ header-navbar/script/script.js
@@ -244,6 +244,45 @@ const renderContactExtendet = async (index) => {
     content.innerHTML += getContactExtended(person, initials, color);
 }
 
+const getContactExtended = (person, initials, color) => {
+    return /*html*/`
+    <div class="slide-in" class="contactExtendDiv">
+        <div class="contactName">
+            <div class="contactPhotoExtendedDiv">
+                <div class="contactInitialsExtended" style="background-color: ${color};">${initials}</div>
+            </div>
+            <div class="rightSectionContact">
+                <div class="contactName">
+                    <p class="personNameExtended">${person.name}</p>
+                </div>
+            <div class="singleContactButtons">
+                <div class="singleContactEdit" onclick="toggleEditOverlay('${person.name}', '${initials}', '${color}')">
+                    <img onclick="renderOverlay()" src="../assets/icon/edit.svg" alt="edit pic">
+                    <p>Edit</p>
+                </div>
+                <div class="singleContactEdit" onclick="deleteContact('${person.name}')">
+                    <img src="../assets/icon/delete.svg" alt="trashcan">
+                    <p>Delete</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="contactInfo">
+
+        </div>
+        <div class="contactInformation">
+        <h3 class="contactInfoHeader">Contact Information</h3>
+        <div class="contactMailAndPhone">
+            <h3>Email</h3>
+            <p class="emailAdress">${person.email}</p>
+            <h3>Phone</h3>
+            <p class="phoneNumber">${person.phone}</p>
+        </div>
+        </div>
+    </div>`
+};
+
+
 const deleteContact = async (person) => {
     const selectedPerson = contactsArray.find(contact => contact.name === person);
 
